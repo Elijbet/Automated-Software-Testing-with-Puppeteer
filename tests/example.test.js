f543a89eb44832d25de70e006bf1794760701af5
Eliza Khachatryan <elijbet@gmail.com>
2022-01-29 09:48:03 -0800

    go back & forward in the browser, interacting with inputs//buttons/checkboxes/dropdown

--- tests/example.test.js
+++ tests/example.test.js
@@ -8,19 +8,36 @@ describe("Learning testing with Puppeteer", () => {
       devtools: false,
     });
     const page = await browser.newPage();
-    // await page.waitForTimeout(3000);
     await page.goto("http://example.com/");
     await page.waitForSelector("h1");
-    await page.reload()
-    await page.waitForTimeout(3000)
-    await page.goto('http://example.com')
+    await page.reload();
+    await page.waitForTimeout(3000);
+    await page.goto("http://example.com");
     await page.waitForSelector("h1");
-    await page.goto("https://dev.to/")
-    await page.waitForSelector("#page-content-inner")
-    await page.goBack()
-    await page.waitForSelector("h1")
-    await page.goForward() //works only in headless mode for some reason
-    await page.waitForSelector("#page-content-inner")
-    await browser.close()
+    await page.goto("https://dev.to/");
+    await page.waitForSelector("#page-content-inner");
+    await page.goBack();
+    await page.waitForSelector("h1");
+    await page.goForward(); //works only in headless mode for some reason
+    await page.waitForSelector("#page-content-inner");
+    await browser.close();
+  });
+  it("should interact with input", async function () {
+    const browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 100,
+      devtools: false,
+    });
+    const page = await browser.newPage();
+    await page.goto("https://devexpress.github.io/testcafe/example");
+    await page.type("#developer-name", "Mike", { delay: 10, button: "left" }); //don't use delay for production use, only for debugging and experimenting
+    await page.click("#background-parallel-testing", { clickCount: 2 });
+    await page.select("#preferred-interface", "JavaScript API");
+    const message = "Lets fill that message with some text";
+    await page.type("#comments", message);
+    await page.click("#submit-button");
+    await page.waitForSelector(".result-content");
+    await page.waitForTimeout(500);
+    browser.close();
   });
 });
