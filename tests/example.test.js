4a084e67ee40ca88a43def62b1745f03a1c1a7a1
Eliza Khachatryan <elijbet@gmail.com>
2022-02-01 18:20:03 -0800

    get page title, URL, element text and count with , assertions with chai

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const expect = require("chai").expect;
 
 describe("Learning testing with Puppeteer", () => {
   it("should launch, reload, goto, goBack, goForward, close the browser", async function () {
@@ -39,5 +40,25 @@ describe("Learning testing with Puppeteer", () => {
     await page.waitForSelector(".result-content");
     await page.waitForTimeout(500);
     browser.close();
+  });
+  it("should interact with page", async function () {
+    const browser = await puppeteer.launch({
+      headless: false,
+      slowMo: 100,
+      devtools: false,
+    });
+    const page = await browser.newPage();
+    await page.goto("http://example.com");
+    const title = await page.title();
+    const url = await page.url();
+    const text = await page.$eval("h1", element => element.textContent);
+    const count = await page.$$eval('p', element => element.length)
+    console.log("Text in h1" + text);
+
+    expect(title).to.be.a("string", "Example Domain");
+    expect(url).to.include("example.com");
+    expect(text).to.be.a("string", "Example Domain");
+    expect(count).to.equal(2);
+    await browser.close();
   });
 });
