92105f3898e84689292194251d984712494f9c45
Eliza Khachatryan <elijbet@gmail.com>
2022-03-26 19:01:53 -0700

    responsive visual tests

--- tests-snapshots/_tests_/visual.test.js
+++ tests-snapshots/_tests_/visual.test.js
@@ -13,15 +13,15 @@ describe("Visual Regression Testing", () => {
   let page;
 
   beforeAll(async function () {
-    browser = await puppeteer.launch({ headless: true });
+    browser = await puppeteer.launch({ headless: false });
     page = await browser.newPage();
+    await page.goto("https://www.example.com");
   });
   afterAll(async function () {
     await browser.close();
   });
 
   test("full Page Snapshot", async function () {
-    await page.goto("https://www.example.com");
     await page.waitForSelector("h1");
     const image = await page.screenshot(); //puppeteer function
     expect(image).toMatchImageSnapshot({
@@ -30,12 +30,39 @@ describe("Visual Regression Testing", () => {
     });
   });
   test("single element snapshot", async function () {
-    await page.goto("https://www.example.com");
     const h1 = await page.waitForSelector("h1");
     const image = await h1.screenshot();
     expect(image).toMatchImageSnapshot({
       failureTresholdType: "percent",
       failureTreshold: 0.01,
     });
+  });
+  test("mobile Snapshot", async function () {
+    await page.waitForSelector("h1");
+    //page.emulate will resize the page. A lot of websites don't expect phones to change size, so you should emulate before navigating to the page.
+    await page.emulate(puppeteer.devices["iPhone X"]);
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      failureTresholdType: "percent",
+      failureTreshold: 0.01,
+    });
+  });
+
+  test("tablet Snapshot", async function () {
+    await page.waitForSelector("h1");
+    await page.emulate(puppeteer.devices["iPad landscape"]);
+    const image = await page.screenshot();
+    expect(image).toMatchImageSnapshot({
+      failureTresholdType: "percent",
+      failureTreshold: 0.01,
+    });
+  });
+  // some elements will throw off your test, such as timestamps or loading icons
+
+  test("remove Element Before Snapshot", async function () {
+    await page.evaluate(() => {
+      (document.querySelectorAll("h1") || []).forEach((el) => el.remove());
+    });
+    await page.waitFor(5000);
   });
 });
